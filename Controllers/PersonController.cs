310d26914c9daf371271343bf3d4226e427d3c2f
Thomashq <thomashenriqq@gmail.com>
2023-08-29 21:16:47 -0300

    TASK3-Remoção de cache

--- Controllers/PersonController.cs
+++ Controllers/PersonController.cs
@@ -5,79 +5,81 @@ using paroquiaRussas.Models;
 using paroquiaRussas.Utility;
 using static paroquiaRussas.Utility.Enum;
 
-namespace paroquiaRussas.Controllers;
-
-[ApiController]
-[Route("api/admin")]
-public class PersonController : Controller
+namespace paroquiaRussas.Controllers
 {
-    private readonly AppDbContext _appDbContext;
 
-    public PersonController(AppDbContext appDbContext)
+    [ApiController]
+    [Route("api/admin")]
+    public class PersonController : Controller
     {
-        _appDbContext = appDbContext;
-    }
+        private readonly AppDbContext _appDbContext;
 
-    [HttpGet]
-    public List<Person> GetAllPerson()
-    {
-        return _appDbContext.Person.ToList();
-    }
-
-    [HttpGet]
-    [Route("getbyid/{id}")]
-    public ActionResult<Person> GetpersonById([FromRoute] long personId)
-    {
-        try
+        public PersonController(AppDbContext appDbContext)
         {
-            PersonTRA personTRA = new(_appDbContext);
-            var person = personTRA.GetPersonById(personId);
-
-            return person;
+            _appDbContext = appDbContext;
         }
-        catch (Exception ex)
+
+        [HttpGet]
+        public List<Person> GetAllPerson()
         {
-            throw new Exception("Erro ao recuperar usuário pelo Id.", ex);
+            return _appDbContext.Person.ToList();
         }
-    }
 
-    [HttpPost]
-    public async Task<IActionResult> Addperson(Person person)
-    {
-        try
+        [HttpGet]
+        [Route("getbyid/{id}")]
+        public ActionResult<Person> GetpersonById([FromRoute] long personId)
         {
-            PasswordEncryption passwordEncryption = new PasswordEncryption(person.Pwd);
-            person.Pwd = passwordEncryption.Encrypt(person.Pwd);
-
-            _appDbContext.Add(person);
-            await _appDbContext.SaveChangesAsync();
-
-            return Ok("Usuário adicionado com sucesso");
-        }
-        catch (Exception ex)
-        {
-            throw ex;
-        }
-    }
-
-    [HttpDelete]
-    public async Task<IActionResult> DeletePerson(int personId)
-    {
-        try
-        {
-            Person person = personId > 0 ? _appDbContext.Person.FirstOrDefault(x => x.Id == personId) : throw new Exception();
-
-            if (person != null)
+            try
             {
-                _appDbContext.Person.Remove(person);
-                await _appDbContext.SaveChangesAsync();
-            }
+                PersonTRA personTRA = new(_appDbContext);
+                var person = personTRA.GetPersonById(personId);
 
-            return Ok("Usuário Deletado com Sucesso");
+                return person;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("Erro ao recuperar usuário pelo Id.", ex);
+            }
         }
-        catch (Exception ex)
+
+        [HttpPost]
+        public async Task<IActionResult> Addperson(Person person)
         {
-            throw ex;
+            try
+            {
+                PasswordEncryption passwordEncryption = new PasswordEncryption(person.Pwd);
+                person.Pwd = passwordEncryption.Encrypt(person.Pwd);
+
+                _appDbContext.Add(person);
+                await _appDbContext.SaveChangesAsync();
+
+                return Ok("Usuário adicionado com sucesso");
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        [HttpDelete]
+        public async Task<IActionResult> DeletePerson(int personId)
+        {
+            try
+            {
+                Person person = personId > 0 ? _appDbContext.Person.FirstOrDefault(x => x.Id == personId) : throw new Exception();
+
+                if (person != null)
+                {
+                    _appDbContext.Person.Remove(person);
+                    await _appDbContext.SaveChangesAsync();
+                }
+
+                return Ok("Usuário Deletado com Sucesso");
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
         }
     }
 }
