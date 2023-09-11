622c0bd59ca429c4bb5fbdd6b898c4bb19641aa1
Thomashq <thomashenriqq@gmail.com>
2023-09-11 16:38:28 -0300

    TASK16 - Remoção dos anuláveis da entidade

--- Models/Event.cs
+++ Models/Event.cs
@@ -4,14 +4,14 @@
     {
         public DateOnly EventDate { get; set; }
 
-        public string? EventTime { get; set; }
+        public string EventTime { get; set; }
 
-        public string? EventName { get; set; }
+        public string EventName { get; set; }
 
-        public string? EventDescription { get; set; }
+        public string EventDescription { get; set; }
 
-        public string? EventImage { get; set; }
+        public string EventImage { get; set; }
 
-        public string? EventAddress { get; set; }
+        public string EventAddress { get; set; }
     }
 }
--- Models/News.cs
+++ Models/News.cs
@@ -2,12 +2,12 @@
 {
     public class News : BaseModel
     {
-        public string? NewsTitle { get; set; }
+        public string NewsTitle { get; set; }
 
-        public string? Headline { get; set; }
+        public string Headline { get; set; }
 
-        public string? NewsContent { get; set; }
+        public string NewsContent { get; set; }
 
-        public string? NewsImage { get; set; }
+        public string NewsImage { get; set; }
     }
 }
