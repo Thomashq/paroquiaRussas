afee16779d6b31bc106addc8b37b3356a4c4307d
Paulo Guilherme <paulogoncalves.dev@gmail.com>
2023-10-24 18:19:22 -0300

    TASK25 - Cria delete para user

--- wwwroot/js/admin.js
+++ wwwroot/js/admin.js
@@ -2,6 +2,7 @@
     validateEqualityOfPasswordFields();
     deleteNews();
     deleteEvents();
+    personEvents();
 });
 
 function clearAllFieldsFromAdmin(conteudo) {
@@ -65,9 +66,33 @@ function deleteNews() {
 //EVENTS
 function deleteEvents() {
     $(".events-delete").click(function () {
-        var newsId = $(this).data("id");
+        var personId = $(this).data("id");
         $.ajax({
-            url: "/api/Events/DeleteEvent/" + newsId,
+            url: "/api/Events/DeleteEvent/" + personId,
+            method: "DELETE",
+            success: function (data) {
+                if (data.message) {
+                    createPopupSuccess(data.message);
+                    location.reload();
+                }
+                else if (data.error)
+                    createPopupError(data.error);
+            },
+            error: function (data) {
+                createPopupError(data.message);
+            }
+        });
+
+        return false;
+    });
+}
+
+//PERSONS
+function personEvents() {
+    $(".person-delete").click(function () {
+        var userId = $(this).data("id");
+        $.ajax({
+            url: "/api/Person/DeletePerson/" + userId,
             method: "DELETE",
             success: function (data) {
                 if (data.message) {
