8a31865dcff5a9aba45d2c913a30d2a9efb5ef51
Thomashq <thomashenriqq@gmail.com>
2023-12-20 17:28:40 -0300

    TASK26-Criação da Schedule controller

---
+++ Controllers/ScheduleController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace paroquiaRussas.Controllers
+{
+    public class ScheduleController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
